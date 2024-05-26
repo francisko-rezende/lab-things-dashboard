58d9d04623510eaf8cca8ed75b0017313c456d9b
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2024-05-26 19:11:28 -0300

    feat: create util for type safe env variables

---
+++ src/config/env.ts
@@ -0,0 +1,25 @@
+import { ZodError, z } from "zod";
+
+const EnvSchema = z.object({
+  OPENWEATHER_KEY: z.string(),
+});
+
+export type EnvSchema = z.infer<typeof EnvSchema>;
+
+try {
+  EnvSchema.parse(process.env);
+} catch (error) {
+  if (error instanceof ZodError) {
+    let message = "Missing required values in .env:\n";
+    error.issues.forEach((issue) => {
+      message += issue.path[0] + "\n";
+    });
+    const e = new Error(message);
+    e.stack = "";
+    throw e;
+  } else {
+    console.error(error);
+  }
+}
+
+export default EnvSchema.parse(process.env);
